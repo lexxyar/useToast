893d5432bd073c6a213d4a45baf3651d251c9306
Aleksey Komov <lexxyar@ya.ru>
2023-10-17 10:53:49 +0300

    * auto add toast container

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import {reactive, UnwrapNestedRefs, UnwrapRef} from "vue";
+import {reactive, render, UnwrapNestedRefs, UnwrapRef, VNode, h} from "vue";
 import IUseToast from "./lib/IUseToast";
 import ToastStruct from "./lib/ToastStruct";
 import IToastStructParams from "./lib/IToastStructParams";
 import {TMessageType} from "./lib/typedef";
+import {Toast} from "@lexxsoft/upui";
 
 const refFlash: UnwrapNestedRefs<IUseToast> = reactive({
     items: [] as ToastStruct[],
@@ -10,9 +11,30 @@ const refFlash: UnwrapNestedRefs<IUseToast> = reactive({
     add: (toast: ToastStruct | IToastStructParams): string => {
         let tst: ToastStruct = (toast instanceof ToastStruct) ? toast : new ToastStruct(toast);
         refFlash.items.unshift(tst)
+
+        let container: HTMLElement | null = document.getElementById('toast-container')
+        if (!container) {
+            container = document.createElement('div')
+            container.setAttribute('id', 'toast-container')
+            container.setAttribute('class', 'fixed right-4 top-4 z-50 w-full max-w-xs space-y-4')
+            document.body.append(container)
+        }
+
+        const tmpContainer: HTMLDivElement = document.createElement('div')
+        const res: VNode<any> = h(Toast, {message: toast.message, styl: toast.type})
+        render(res, tmpContainer)
+        const item: HTMLElement = tmpContainer.children.item(0) as HTMLElement
+        item.setAttribute('id', tst.id)
+        container.append(item)
+
+        setTimeout(() => {
+            refFlash.dismiss(tst.id)
+        }, 3000)
+
         return tst.id
     },
     error: (message: string): string => {
+        console.log('error')
         return refFlash.add({message, type: 'danger'})
     },
     info: (message: string): string => {
